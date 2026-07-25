72a2a0d52c920989d32044870fd70351bb4e1f18
capital-poeeisan <captial.poeeieisan@gmail.com>
2026-07-25 17:00:17 +0630

    Ignore bin and obj

--- WEB_TENNIC/Interface/Repositories/ImportExcel/IImportExcelRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using WEB_TENNIC.Models.ViewModels;
-
-namespace WEB_TENNIC.Interface.Repositories.ImportExcel
-{
-    public interface IImportExcelRepository
-    {
-         Task<int> ImportExcelAsync(ImportExcelViewModel m);
-    }
-}
--- WEB_TENNIC/Models/ViewModels/ProjectStaffViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WEB_TENNIC.Models.ViewModels
-{
-    public class ProjectStaffViewModel
-    {
-        public string StaffCD { get; set; }
-
-        public string StaffName { get; set; }
-    }
-}
