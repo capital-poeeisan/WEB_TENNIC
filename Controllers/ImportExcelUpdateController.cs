d75c0f87a7c78098e98444d94514f6ed52670e55
Khin Sandar Thein <capital.khinsandarthein@gmail.com>
2026-09-15 13:36:19 +0630

    ksdt

--- Controllers/ImportExcelUpdateController.cs
+++ Controllers/ImportExcelUpdateController.cs
@@ -223,7 +223,7 @@ namespace WEB_TENNIC.Controllers
                             success = false,
                             type = "error",
                             message =
-                                  $"'{model.fileName.FileName}'Excelファイルにデータがありません。"
+                                  "Excelファイルにデータがありません。"
                         });
                     }
 
